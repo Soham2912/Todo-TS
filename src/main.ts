fe18fb88a0d3016de11aa6d0f8a49ed8db840372
Soham2912 <sohamlahoti@gmail.com>
2024-07-25 12:30:35 +0530

    feat: Add input field name attribute for todo title

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,35 @@
 import './style.css'
 
+interface Todo{
+  title:string;
+  isCompleted:boolean;
+  readonly id : string;
+}
+
+const todos:Array<Todo>=[];
+
+const todoContainer= document.querySelector(".todoContainer") as HTMLDivElement;
+
+const todoInput=  document.getElementsByName("title")[0] as HTMLInputElement;
+
+const myForm = document.getElementById("myForm") as HTMLFormElement;
+
+myForm.onsubmit = (e) =>{
+  e.preventDefault();
+  const todo :Todo = {
+    title:todoInput.value,
+    isCompleted:false,
+    id:String(Math.random()*100),
+  };
+  todos.push(todo);
+renderTodo(todos);
+
+}
+
+
+const renderTodo= (todos:Todo[]) =>{
+      todos.forEach(item=>{
+        generateTodo();
+      })
+
+}
